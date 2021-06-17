593bdd72bafe4dd500780df19bfb47d3655d8ebd
PrahaladhN <70474273+PrahaladhN@users.noreply.github.com>
2021-06-17 12:08:35 +0530

    Update hello.java

---
+++ javaCode/hello.java
@@ -0,0 +1,15 @@
+package learnJava;
+
+public class Hello {
+    public static void main(String[] args) {
+        helloWorld();
+    }
+
+    public void helloWord() {
+        System.out.println("Hello, World!");
+    }
+
+    public void greeting(String name) {
+        System.out.println("Hey, How are you doing?." +  name);
+    }
+}
